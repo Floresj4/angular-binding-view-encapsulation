4035fe74e5cf3b40a6a51a5d25403c33648a019e
Jason <floresj4@gmail.com>
2017-10-18 19:19:14 -0400

    adding game controller created from cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GameControllerComponent } from './game-controller/game-controller.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameControllerComponent
   ],
   imports: [
     BrowserModule
