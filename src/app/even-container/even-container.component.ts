913bc87b8b6ad8d616d6f29badb776e86c095947
Jason <floresj4@gmail.com>
2017-10-23 19:29:18 -0400

    completed even-component

--- src/app/even-container/even-container.component.ts
+++ src/app/even-container/even-container.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-even-container-component',
   templateUrl: './even-container.component.html',
-  styleUrls: ['./even-container.component.css']
+  styleUrls: ['./even-container.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class EvenContainerComponent {
 
