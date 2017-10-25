9ab507c673d2ac0151f27998a32463a6965c461b
Jason <floresj4@gmail.com>
2017-10-24 21:23:39 -0400

    adding distribution as docs directory

---
+++ docs/main.bundle.js
@@ -0,0 +1,387 @@
+webpackJsonp(["main"],{
+
+/***/ "../../../../../src/$$_gendir lazy recursive":
+/***/ (function(module, exports) {
+
+function webpackEmptyAsyncContext(req) {
+	// Here Promise.resolve().then() is used instead of new Promise() to prevent
+	// uncatched exception popping up in devtools
+	return Promise.resolve().then(function() {
+		throw new Error("Cannot find module '" + req + "'.");
+	});
+}
+webpackEmptyAsyncContext.keys = function() { return []; };
+webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
+module.exports = webpackEmptyAsyncContext;
+webpackEmptyAsyncContext.id = "../../../../../src/$$_gendir lazy recursive";
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<div class='container'>\r\n\t<div class='row'>\r\n\t  <app-game-controller (counterIncremented)=\"onIncrement($event)\">\r\n\t  </app-game-controller>\r\n\t</div>\r\n\t\r\n\t<div class='row'>\r\n\t\t<div class='col-xs-6'>\r\n\t\t\t<div class='row'>\r\n\t\t\t\t<h4>Odd Numbers</h4>\r\n\t\t\t\t<app-odd-container-component\r\n\t\t\t\t  [data-value]=\"odd\">\r\n\t\t\t\t</app-odd-container-component>\r\n\t\t\t</div>\r\n\t\t</div>\r\n\t\t\r\n\t\t<div class='col-xs-6'>\r\n\t\t\t<div class='row'>\r\n\t\t\t  <app-even-container-component>\r\n\t\t\t\t  <div *ngFor=\"let d of even\"\r\n                    class='col-xs-2 col-sm-2 number-block'>\r\n                      {{ d }}\r\n                  </div>\r\n\t\t\t  </app-even-container-component>\r\n\t\t\t</div>\r\n\t\t</div>\r\n\t</div>\r\n</div>"
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var AppComponent = (function () {
+    function AppComponent() {
+        this.odd = [];
+        this.even = [];
+    }
+    AppComponent.prototype.onIncrement = function (event) {
+        event.count % 2 == 0
+            ? this.even.push(event.count)
+            : this.odd.push(event.count);
+    };
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+        selector: 'app-root',
+        template: __webpack_require__("../../../../../src/app/app.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/app.component.css")],
+        encapsulation: __WEBPACK_IMPORTED_MODULE_0__angular_core__["_15" /* ViewEncapsulation */].None
+    })
+], AppComponent);
+
+//# sourceMappingURL=app.component.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__("../../../platform-browser/@angular/platform-browser.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_component__ = __webpack_require__("../../../../../src/app/app.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__game_controller_game_controller_component__ = __webpack_require__("../../../../../src/app/game-controller/game-controller.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__odd_container_odd_container_component__ = __webpack_require__("../../../../../src/app/odd-container/odd-container.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__even_container_even_container_component__ = __webpack_require__("../../../../../src/app/even-container/even-container.component.ts");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+
+
+var AppModule = (function () {
+    function AppModule() {
+    }
+    return AppModule;
+}());
+AppModule = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["L" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */],
+            __WEBPACK_IMPORTED_MODULE_3__game_controller_game_controller_component__["a" /* GameControllerComponent */],
+            __WEBPACK_IMPORTED_MODULE_4__odd_container_odd_container_component__["a" /* OddContainerComponent */],
+            __WEBPACK_IMPORTED_MODULE_5__even_container_even_container_component__["a" /* EvenContainerComponent */]
+        ],
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */]
+        ],
+        providers: [],
+        bootstrap: [__WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */]]
+    })
+], AppModule);
+
+//# sourceMappingURL=app.module.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/even-container/even-container.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/even-container/even-container.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<h4>Even Component</h4>\r\n<ng-content></ng-content>"
+
+/***/ }),
+
+/***/ "../../../../../src/app/even-container/even-container.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return EvenContainerComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var EvenContainerComponent = (function () {
+    function EvenContainerComponent() {
+    }
+    EvenContainerComponent.prototype.ngOnInit = function () {
+    };
+    return EvenContainerComponent;
+}());
+EvenContainerComponent = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+        selector: 'app-even-container-component',
+        template: __webpack_require__("../../../../../src/app/even-container/even-container.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/even-container/even-container.component.css")],
+        encapsulation: __WEBPACK_IMPORTED_MODULE_0__angular_core__["_15" /* ViewEncapsulation */].Emulated
+    }),
+    __metadata("design:paramtypes", [])
+], EvenContainerComponent);
+
+//# sourceMappingURL=even-container.component.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/game-controller/game-controller.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/game-controller/game-controller.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<div class='row'>\n\t<div class='col-md-12'>\n\t\t<h4>Number Controller</h4>\n\t</div>\n</div>\n<div class='row'>\n\t<div class='col-md-12'>\n\t\t<button\n\t\t\tclass='btn btn-success'\n\t\t\t(click)=\"onStartCounter()\">Start</button>\n\t\t<button\n\t\t\tclass='btn btn-danger'\n\t\t\t(click)=\"onStopCounter()\">Stop</button>\n\t</div>\n</div>"
+
+/***/ }),
+
+/***/ "../../../../../src/app/game-controller/game-controller.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return GameControllerComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var GameControllerComponent = (function () {
+    function GameControllerComponent() {
+        this.count = 0;
+        this.counterIncremented = new __WEBPACK_IMPORTED_MODULE_0__angular_core__["w" /* EventEmitter */]();
+    }
+    GameControllerComponent.prototype.isRunning = function () {
+        return this.intervalId !== null;
+    };
+    GameControllerComponent.prototype.ngOnInit = function () {
+    };
+    GameControllerComponent.prototype.onStartCounter = function () {
+        var _this = this;
+        //set interval which emits the incremented count
+        this.intervalId = setInterval(function () {
+            console.log(++_this.count);
+            _this.counterIncremented.emit({ count: _this.count });
+        }, 1000);
+    };
+    GameControllerComponent.prototype.onStopCounter = function () {
+        clearInterval(this.intervalId);
+    };
+    return GameControllerComponent;
+}());
+__decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["R" /* Output */])(),
+    __metadata("design:type", Object)
+], GameControllerComponent.prototype, "counterIncremented", void 0);
+GameControllerComponent = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+        selector: 'app-game-controller',
+        template: __webpack_require__("../../../../../src/app/game-controller/game-controller.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/game-controller/game-controller.component.css")]
+    }),
+    __metadata("design:paramtypes", [])
+], GameControllerComponent);
+
+//# sourceMappingURL=game-controller.component.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/odd-container/odd-container.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, ".number-block {\r\n\tbackground-color:#ff4000;\r\n\tcolor:#fff;\r\n\tborder-radius:5px;\r\n\tborder: 1px solid #000;\r\n\tmargin-right:5px;\r\n\tmargin-bottom:5px;\r\n\ttext-align:center;\r\n}", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/odd-container/odd-container.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<div *ngFor=\"let d of data\"\r\n\t class='col-xs-2 col-sm-2 number-block'>\r\n\t{{ d }}\r\n</div>"
+
+/***/ }),
+
+/***/ "../../../../../src/app/odd-container/odd-container.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return OddContainerComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var OddContainerComponent = (function () {
+    function OddContainerComponent() {
+    }
+    OddContainerComponent.prototype.ngOnInit = function () {
+    };
+    return OddContainerComponent;
+}());
+__decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["E" /* Input */])('data-value'),
+    __metadata("design:type", Object)
+], OddContainerComponent.prototype, "data", void 0);
+OddContainerComponent = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+        selector: 'app-odd-container-component',
+        template: __webpack_require__("../../../../../src/app/odd-container/odd-container.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/odd-container/odd-container.component.css")]
+    }),
+    __metadata("design:paramtypes", [])
+], OddContainerComponent);
+
+//# sourceMappingURL=odd-container.component.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/environments/environment.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+// The file contents for the current environment will overwrite these during build.
+var environment = {
+    production: false
+};
+//# sourceMappingURL=environment.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/main.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__ = __webpack_require__("../../../platform-browser-dynamic/@angular/platform-browser-dynamic.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_app_module__ = __webpack_require__("../../../../../src/app/app.module.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__("../../../../../src/environments/environment.ts");
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_19" /* enableProdMode */])();
+}
+Object(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */])
+    .catch(function (err) { return console.log(err); });
+//# sourceMappingURL=main.js.map
+
+/***/ }),
+
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__("../../../../../src/main.ts");
+
+
+/***/ })
+
+},[0]);
+//# sourceMappingURL=main.bundle.js.map
